be49288e95ae237fb4852155368ab09a14b771ee
Darana Sukma Vidya <darana.sv@gmail.com>
2024-04-23 15:27:02 +0700

    change api project

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,10 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectModel } from 'nest-knexjs';
 import { CreateProjectDto } from './dto/create-project.dto';
@@ -17,13 +23,34 @@ export class ProjectsService {
     return `This action returns a #${id} project`;
   }
 
+  async findProjectName(name?: string) {
+    try {
+      const projectName = await this.knex
+        .table('projects')
+        .where('name', name)
+        .first();
+      return projectName;
+    } catch (error) {
+      throw error || new InternalServerErrorException();
+    }
+  }
+
   async create(createProjectDto: CreateProjectDto) {
     try {
+      const findName = await this.findProjectName(createProjectDto.name);
+      if (findName) {
+        throw new BadRequestException('Project name is already exist');
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const projects = await this.knex.table('projects').insert({
         name: createProjectDto.name,
       });
 
-      return { projects };
+      return {
+        status: 200,
+        message: 'Project is successfully created',
+      };
     } catch (err) {
       throw new HttpException(err, HttpStatus.BAD_REQUEST);
     }
